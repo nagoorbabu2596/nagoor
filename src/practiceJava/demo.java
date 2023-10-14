c119cb1aeaa69be94acf6d835d0b2a4dbea56f52
nagoorbabu2596 <nagoor2596@gmail.com>
2023-10-14 13:53:48 +0530

    new changes in kiran branch

--- src/practiceJava/demo.java
+++ src/practiceJava/demo.java
@@ -1,5 +1,7 @@
 package practiceJava;
 
+import org.testng.annotations.Test;
+
 public class demo {
 
 	public static void main(String[] args) {
@@ -7,4 +9,16 @@ public class demo {
 
 	}
 
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3");
+	}
 }
