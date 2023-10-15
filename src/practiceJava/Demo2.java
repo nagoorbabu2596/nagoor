304444111f24ad3269faabfe33da81592a26aa6e
nagoorbabu2596 <nagoor2596@gmail.com>
2023-10-15 12:27:13 +0530

    kinger

--- src/practiceJava/Demo2.java
+++ src/practiceJava/Demo2.java
@@ -5,7 +5,13 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New thing my things");
+<<<<<<< HEAD
 		
+=======
+		System.out.println("added code");
+		System.out.println("king is back");
+		System.out.println("new");
+>>>>>>> 129f8a082a93e9696ae0e020f48ff33ebdd39e1c
 	}
 
 }
